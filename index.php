3c1b4d7b7702fcbf802ca4fc8d4c61b7c791bbf6
CTBest744 <tkddnjs744@gmail.com>
2021-03-08 16:14:16 +0900

    .

--- index.php
+++ index.php
@@ -1,8 +1,23 @@
 <!DOCTYPE html>
 <html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
   <body>
+    <h1>WEB</h1>
+    <ol>
+      <li><a href="index.php?id=HTML">HTML</a></li>
+      <li><a href="index.php?id=CSS">CSS</a></li>
+      <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+      <?php
+        echo $_GET['id'];
+      ?>
+    </h2>
     <?php
-      echo date('Y-m-d H:i:s');
-    ?>
+    echo file_get_contents("data/".$_GET['id']);
+     ?>
   </body>
 </html>
